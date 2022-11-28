4b4077900816b2e42ef318a5e7bc6987872fa223
chetan <ctidgam1997@gmail.com>
2022-11-28 21:23:29 +0530

    chek

--- src/Component/Login/Login.jsx
+++ src/Component/Login/Login.jsx
@@ -83,7 +83,7 @@ export default function Login() {
         initialFocusRef={initialRef}
         finalFocusRef={finalRef}
         isOpen={temp===true}
-        onClose={onClose}
+        onClose={secModal}
       >
         <ModalOverlay />
         <ModalContent>
