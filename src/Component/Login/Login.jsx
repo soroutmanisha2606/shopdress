c4f6c1b8225ec98f1a6480362d81b6c30d7bea14
chetan <ctidgam1997@gmail.com>
2022-11-29 11:21:01 +0530

    login

--- src/Component/Login/Login.jsx
+++ src/Component/Login/Login.jsx
@@ -16,22 +16,33 @@ import {
   Box,
   Flex,
   Spacer,
+  Stack,
+  Checkbox,
+  useColorModeValue,
+  HStack,
+  InputRightElement,
+  InputGroup,
+  Text,
 } from '@chakra-ui/react'
 import { FaUser } from "react-icons/fa";
 import { Link } from 'react-router-dom';
+import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
+import CaptionCarousel from './CaptionCarousel';
 export default function Login() {
-  const { isOpen, onOpen, onClose } = useDisclosure()
-  const [temp,setTemp]=useState(false)
+  const [showPassword, setShowPassword] = useState(false);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { isOpen: isOpen2, onOpen: onOpen2, onClose: onClose2, } = useDisclosure();
+  const [temp, setTemp] = useState(false)
 
   const initialRef = React.useRef(null)
   const finalRef = React.useRef(null)
 
-  const secModal = ()=>{
-    onClose()
-    setTimeout(() => {
-      setTemp(!temp)
-    }, 100);
-  }
+  // const secModal = ()=>{
+  //   onClose()
+  //   setTimeout(() => {
+  //     setTemp(!temp)
+  //   }, 100);
+  // }
   return (
     <>
       <Button onClick={onOpen}><FaUser /></Button>
@@ -40,38 +51,68 @@ export default function Login() {
         finalFocusRef={finalRef}
         isOpen={isOpen}
         onClose={onClose}
+        size={'xl'}
       >
         <ModalOverlay />
         <ModalContent>
           <ModalHeader>Login</ModalHeader>
           <ModalCloseButton />
           <ModalBody pb={6}>
-            <FormControl>
-              <FormLabel>EMAIL</FormLabel>
-              <Input placeholder='Enter Email' />
-            </FormControl>
-
-            <FormControl mt={4}>
-              <Flex>
-                <Box>
-                <FormLabel>PASSWORD</FormLabel>
-                </Box>
-                <Spacer />
-                <Box >
-                <Link style={{color:'#3182ce'}}>Forgot Password?</Link>
-                </Box>
-              </Flex>
-              <Box>
+            <Flex>
+            <Box
+              bg={useColorModeValue('white', 'gray.700')}
+              p={8}>
+              <Stack spacing={4}>
+                <FormControl id="email">
+                  <FormLabel>Email address</FormLabel>
+                  <Input type="email" />
+                </FormControl>
+                <FormControl id="password">
+                  <FormLabel>Password</FormLabel>
+                  <Input type="password" />
+                </FormControl>
+                <Stack spacing={10}>
+                  <Stack
+                    direction={{ base: 'column', sm: 'row' }}
+                    align={'start'}
+                    justify={'space-between'}>
+                    <Checkbox>Remember me</Checkbox>
+                    <Link color={'blue.400'}>Forgot password?</Link>
+                  </Stack>
+                  <Button
+                    bg={'blue.400'}
+                    color={'white'}
+                    _hover={{
+                      bg: 'blue.500',
+                    }}>
+                    Sign in
+                  </Button>
+                  <Stack pt={6}>
+                    <Text align={'center'}>
+                      Don't have a account? <Link onClick={() => {
+                        onClose(); setTimeout(() => {
+                          onOpen2()
+                        }, 100);
+                      }} color={'blue.400'}>Sign Up</Link>
+                    </Text>
+                  </Stack>
+                </Stack>
+              </Stack>
+            </Box>
+            <Box>
+              {/* <CaptionCarousel/> */}
               </Box>
-              <Input placeholder='Enter Password' />
-            </FormControl>
+            </Flex>
           </ModalBody>
 
           <ModalFooter>
-            <Button colorScheme='blue' mr={3}>
+            {/* <Button colorScheme='blue' mr={3}>
               Login
             </Button>
-            <Button onClick={secModal}>Cancel</Button>
+            <Button onClick={onOpen2}>Cancel</Button> */}
+            <Box>
+
+            </Box>
           </ModalFooter>
         </ModalContent>
       </Modal>
@@ -79,43 +120,77 @@ export default function Login() {
 
 
 
-      <Modal
-        initialFocusRef={initialRef}
-        finalFocusRef={finalRef}
-        isOpen={temp===true}
-        onClose={secModal}
-      >
+      <Modal initialFocusRef={initialRef} finalFocusRef={finalRef} isOpen={isOpen2} onClose={onClose2}>
         <ModalOverlay />
         <ModalContent>
           <ModalHeader>Signup</ModalHeader>
           <ModalCloseButton />
           <ModalBody pb={6}>
-            <FormControl>
-              <FormLabel>EMAIL</FormLabel>
-              <Input placeholder='Enter Email' />
-            </FormControl>
-
-            <FormControl mt={4}>
-              <Flex>
-                <Box>
-                <FormLabel>PASSWORD</FormLabel>
-                </Box>
-                <Spacer />
-                <Box >
-                <Link style={{color:'#3182ce'}}>Forgot Password?</Link>
-                </Box>
-              </Flex>
-              <Box>
-              </Box>
-              <Input placeholder='Enter Password' />
-            </FormControl>
+            <Box>
+              <Stack spacing={4}>
+                <HStack>
+                  <Box>
+                    <FormControl id="firstName" isRequired>
+                      <FormLabel>First Name</FormLabel>
+                      <Input type="text" />
+                    </FormControl>
+                  </Box>
+                  <Box>
+                    <FormControl id="lastName">
+                      <FormLabel>Last Name</FormLabel>
+                      <Input type="text" />
+                    </FormControl>
+                  </Box>
+                </HStack>
+                <FormControl id="email" isRequired>
+                  <FormLabel>Email address</FormLabel>
+                  <Input type="email" />
+                </FormControl>
+                <FormControl id="password" isRequired>
+                  <FormLabel>Password</FormLabel>
+                  <InputGroup>
+                    <Input type={showPassword ? 'text' : 'password'} />
+                    <InputRightElement h={'full'}>
+                      <Button
+                        variant={'ghost'}
+                        onClick={() =>
+                          setShowPassword((showPassword) => !showPassword)
+                        }>
+                        {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                      </Button>
+                    </InputRightElement>
+                  </InputGroup>
+                </FormControl>
+                <Stack spacing={10} pt={2}>
+                  <Button
+                    loadingText="Submitting"
+                    size="lg"
+                    bg={'blue.400'}
+                    color={'white'}
+                    _hover={{
+                      bg: 'blue.500',
+                    }}>
+                    Sign up
+                  </Button>
+                </Stack>
+                <Stack pt={6}>
+                  <Text align={'center'}>
+                    Already a user? <Link onClick={() => {
+                      onClose2(); setTimeout(() => {
+                        onOpen()
+                      }, 100);
+                    }} color={'blue.400'}>Login</Link>
+                  </Text>
+                </Stack>
+              </Stack>
+            </Box>
           </ModalBody>
 
           <ModalFooter>
             <Button colorScheme='blue' mr={3}>
               Login
             </Button>
-            <Button onClick={secModal}>Cancel</Button>
+            <Button onClick={onClose2}>Cancel</Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
@@ -123,7 +198,7 @@ export default function Login() {
 
 
 
-      
+
     </>
   )
 
